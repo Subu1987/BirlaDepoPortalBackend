973705bf916efd14374a8dc9131b5342eb106a12
Subu1987 <subrotosaha1987@gmail.com>
2025-11-14 12:01:39 +0530

    connection to the mysql password hardcoded

--- connections/connection.js
+++ connections/connection.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: parseInt(process.env.MYSQL_PORT, 10),
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: "birla@admin12#",
   database: process.env.MYSQL_DATABASE,
   multipleStatements: process.env.MYSQL_MULTIPLE_STATEMENTS === "true",
   connectTimeout: 20000, // 20 seconds
